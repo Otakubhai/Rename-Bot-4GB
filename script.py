d452f460dda308e25344d6e1a6f38ac9b1bea03f
Otakubhai <87870903+Otakubhai@users.noreply.github.com>
2024-02-27 19:43:03 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,49 +1,49 @@
 class script(object):
-    HELP_TXT = """<b>Hey</b> {}
+    HELP_TXT = """<b>Wassup</b> {}
     
-<b>Here Is The Help For My Commands.</b>"""
+<b>Here Is The List of My Commands.</b>"""
 
-    CAPTION_TXT = """<b><u>📝  HOW TO SET CAPTION</u></b>
+    CAPTION_TXT = """<b><u>📝  HOW TO SET THE CAPTION</u></b>
 
-<b>⦿ /set_caption - Use This Command To Set Your Caption</b>
-<b>⦿ /see_caption - Use This Command To See Your Caption</b>
-<b>⦿ /del_caption - Use This Command To Delete Your Caption</b>"""
+<b>➢ /set_caption - Use This Command To Set Your Caption</b>
+<b>➢ /see_caption - Use This Command To See Your Caption</b>
+<b>➢ /del_caption - Use This Command To Delete Your Caption</b>"""
    
     THUMBNAIL_TXT = """<b><u>🖼️  HOW TO SET THUMBNAIL</u></b>
 
-<b>⦿ You Can Add Custom Thumbnail Simply By Sending A Photo To Me....</b>
+<b>➢ You Can Add Custom Thumbnail Simply By Sending A Photo To Me....</b>
 
-<b>⦿ /viewthumb - Use This Command To See Your Thumbnail</b>
-<b>⦿ /delthumb - Use This Command To Delete Your Thumbnail</b>"""
+<b>➢ /viewthumb - Use This Command To See Your Thumbnail</b>
+<b>➢ /delthumb - Use This Command To Delete Your Thumbnail</b>"""
 
     ABOUT_TXT = """<b>🤖 My Name :</b> <a href='https://t.me/FileRenameXProBot'>Rename Bot 4GB</a>
 <b>📝 Language :</b> <a href='https://python.org'>Python 3</a>
 <b>📚 Library :</b> <a href='https://pyrogram.org'>Pyrogram 2.0</a>
-<b>🚀 Server :</b> <a href='https://heroku.com'>Heroku</a>
-<b>📢 Channel :</b> <a href='https://t.me/Madflix_Bots'>Madflix Botz</a>
-<b>🧑‍💻 Developer :</b> <a href='https://t.me/CallAdminRobot'>Jishu Developer</a>
+<b>📊 Server :</b> <a href='https://heroku.com'>Heroku</a>
+<b>✨ Channel :</b> <a href='https://t.me/Madflix_Bots'>Madflix Botz</a>
+<b>👷🏻‍♂️ Developer :</b> <a href='https://t.me/CallAdminRobot'>Jishu Developer</a>
 
 <b>♻️ Bot Made By :</b> @Madflix_Bots"""
 
     DONATE_TXT = """
-<b>🥲 Thanks For Showing Interest In Donation! ❤️</b>
+<b>🙇🏻‍♂️ Thanks For Showing Interest In Donation! ❤️</b>
 
-If You Like My Bots & Projects, You Can 🎁 Donate Me Any Amount From 10 Rs Upto Your Choice.
+If you appreciate my bots & projects, feel free to 🎁 donate any amount from 5/10 Rs up to your choice!
 
-<b>🛍 UPI ID:</b> <code>madflixofficial@axl</code> 
+<b>🇮🇳 UPI ID:</b> <code>ogbhargav@fam</code> 
 
 <b>📸 Send Screenshot After Donation To Activate Your Premium Plan </b> """
 
     ADMIN_TXT = """<b><u>🦋 ADMIN ALL COMMANDS HERE</u></b>
 
-<b>⦿ /users - Use This Command To See Total Users</b>
-<b>⦿ /allids - Use This Command To See All Users IDs</b>
-<b>⦿ /broadcast - Use This Command To Send A Message To Users</b>
-<b>⦿ /warn - Use This Command To Send A Message To A User</b>
-<b>⦿ /resetpower - Use This Command To Reset User Power</b>
-<b>⦿ /ceasepower - Use This Command To Cease User Power</b>
-<b>⦿ /addpremium - Use This Command To Add Premium To Users</b>
-<b>⦿ /restart - Use This Command To Cancel All Process And Restart The Bot</b>"""
+<b>➢ /users - Use This Command To See Total Users</b>
+<b>➢ /allids - Use This Command To See All Users IDs</b>
+<b>➢ /broadcast - Use This Command To Send A Message To Users</b>
+<b>➢ /warn - Use This Command To Send A Message To A User</b>
+<b>➢ /resetpower - Use This Command To Reset User Power</b>
+<b>➢ /ceasepower - Use This Command To Cease User Power</b>
+<b>➢ /addpremium - Use This Command To Add Premium To Users</b>
+<b>➢ /restart - Use This Command To Cancel All Process And Restart The Bot</b>"""
 
 
 
